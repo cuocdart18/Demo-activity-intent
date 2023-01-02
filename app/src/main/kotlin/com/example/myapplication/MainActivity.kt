2d84eecfd82c2201f02c1005683594a04afdc6f4
Dat Cuoc <ngoquocdat203@gmail.com>
2023-01-02 12:36:44 +0700

    main -> second -> main with result

--- app/src/main/kotlin/com/example/myapplication/MainActivity.kt
+++ app/src/main/kotlin/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,22 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
     private lateinit var binding: ActivityMainBinding
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == 2003) {
+                val intent = result.data
+                val bundle = intent?.extras
+                val data = bundle?.get("data_object_2")
+                Log.w(TAG, "data 2 = $data")
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +29,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun passDataToSecondActivity() {
         val data = User("em anh Tung", 20)
-        Log.w(TAG, "data = $data")
+        Log.w(TAG, "data 1 = $data")
 
         val intent = Intent(this, SecondActivity::class.java)
         val bundle = Bundle()
-        bundle.putSerializable("data_object", data)
+        bundle.putSerializable("data_object_1", data)
         intent.putExtras(bundle)
-        startActivity(intent)
+
+        activityResultLauncher.launch(intent)
     }
 }
--- app/src/main/kotlin/com/example/myapplication/SecondActivity.kt
+++ app/src/main/kotlin/com/example/myapplication/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -17,11 +18,19 @@ class SecondActivity : AppCompatActivity() {
 
         // get data from main activity
         val bundle = intent.extras
-        val data = bundle?.get("data_object")
-        Log.w(TAG, "data = $data")
+        val data = bundle?.get("data_object_1")
+        Log.w(TAG, "data 1 = $data")
     }
 
     private fun passDataBackToMainActivity() {
-        TODO("Not yet implemented")
+        val data = User("em anh Giang", 20)
+        Log.w(TAG, "data 2 = $data")
+
+        val intent = Intent(this, MainActivity::class.java)
+        val bundle = Bundle()
+        bundle.putSerializable("data_object_2", data)
+        intent.putExtras(bundle)
+        setResult(2003, intent)
+        finish()
     }
 }
