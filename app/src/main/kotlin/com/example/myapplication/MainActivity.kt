0c89a0ead978316c85eb99ad9e5574ce78c31a6d
Dat Cuoc <ngoquocdat203@gmail.com>
2023-01-02 11:45:54 +0700

    main to second

---
+++ app/src/main/kotlin/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnPassData.setOnClickListener { passDataToSecondActivity() }
+    }
+
+    private fun passDataToSecondActivity() {
+        val data = "happy new year"
+        Log.w(TAG, "data = $data")
+
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("data", data)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/kotlin/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private val TAG = SecondActivity::class.java.simpleName
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnRepassData.setOnClickListener { passDataBackToMainActivity() }
+
+        // get data from main activity
+        val data = intent.getStringExtra("data")
+        Log.w(TAG, "data = $data")
+    }
+
+    private fun passDataBackToMainActivity() {
+        TODO("Not yet implemented")
+    }
+}
